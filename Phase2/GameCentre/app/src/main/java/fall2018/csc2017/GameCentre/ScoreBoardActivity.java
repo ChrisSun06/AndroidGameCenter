bbd63c5f42d565a65ce013b9c4e52ea773b4797e
MingJin Lu <mingjin.lu@mail.utoronto.ca>
2018-11-18 17:40:26 -0500

    connecting sliding tile buttons to sliding tile fragment

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ScoreBoardActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ScoreBoardActivity.java
@@ -61,21 +61,6 @@ public class ScoreBoardActivity extends AppCompatActivity {
 
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 
     /**
@@ -100,13 +85,13 @@ public class ScoreBoardActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
+            // Show 5 total pages.
             return 5;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            // Show 3 total pages.
+            // Show 5 total pages.
             return UserAccount.games[position];
         }
 
--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingTileFragment.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingTileFragment.java
@@ -1,11 +1,20 @@
 package fall2018.csc2017.GameCentre;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -13,17 +22,123 @@ import android.view.ViewGroup;
  */
 public class SlidingTileFragment extends Fragment {
 
+    /**
+     * A temporary save file.
+     */
+    public static final String TEMP_SAVE_FILENAME = "save_file_tmp.ser";
+
+    /**
+     * The board manager.
+     */
+    private BoardManager boardManager;
+
+    /**
+     * The buttons to display.
+     */
+    private HashMap<String, BoardManager> gameStateMap = new HashMap<>();
+
+    /**
+     * Current Game
+     */
+    public static int CURRENT_GAME;
 
     public SlidingTileFragment() {
         // Required empty public constructor
+
     }
 
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sliding_tile, container, false);
+        view = inflater.inflate(R.layout.fragment_sliding_tile, container, false);
+        add3x3ButtonListener();
+        add4x4ButtonListener();
+        add5x5ButtonListener();
+        return view;
+
     }
 
+
+    /**
+     * Activate the 3x3 game.
+     */
+    private void add3x3ButtonListener() {
+        Button by3sliding = view.findViewById(R.id.by3sliding);
+        by3sliding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activateGame(3);
+            }
+        });
+    }
+
+
+    /**
+     * Activate the 4x4 game.
+     */
+    private void add4x4ButtonListener() {
+        Button by4sliding = view.findViewById(R.id.by4sliding);
+        by4sliding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activateGame(4);
+            }
+        });
+    }
+
+    /**
+     * Activate the 5x5 game.
+     */
+    private void add5x5ButtonListener() {
+        Button by5sliding = view.findViewById(R.id.by5sliding);
+        by5sliding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activateGame(5);
+            }
+        });
+    }
+
+    /**
+     * Switch to the GameActivity view to play the game.
+     */
+    private void switchToGame() {
+        Intent tmp = new Intent(getActivity(), GameActivity.class);
+        saveToFile(TEMP_SAVE_FILENAME);
+        startActivity(tmp);
+    }
+
+    /**
+     * Activate the ixi game.
+     *
+     * @param i the complexity of game
+     */
+    private void activateGame(int i) {
+        boardManager = new BoardManager(i);
+        boardManager.getBoard().setMaxUndoTime(LoginActivity.accManager.
+                getUserUndoTime(getActivity().getApplicationContext()));
+        CURRENT_GAME = i;
+        switchToGame();
+    }
+
+
+    /**
+     * Save the game state to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    public void saveToFile(String fileName) {
+        gameStateMap.put(LoginActivity.currentUser, boardManager);
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    getActivity().openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(gameStateMap);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
 }
