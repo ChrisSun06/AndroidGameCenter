42438e517505f3c1a2df6ced966bb505a9a40f49
Jiongtian Guo <jiongtian.guo@mail.utoronto.ca>
2018-11-19 01:34:09 -0500

    "rewriting 2048 board manager. Unfinished"

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/The2048BoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/The2048BoardManager.java
@@ -2,8 +2,11 @@ package fall2018.csc2017.GameCentre;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import fall2018.csc2017.GameCentre.tiles.TofeTile;
 
 /**
  * Manage a board, including swapping tiles, checking for a win, and managing taps.
@@ -13,103 +16,101 @@ class The2048BoardManager implements Serializable {
     /**
      * The board being managed.
      */
-    private Board board;
+    private The2048Board board;
 
     /**
      * Manage a board that has been pre-populated.
+     *
      * @param board the board
      */
-    BoardManager(Board board) {
+    The2048BoardManager(The2048Board board) {
         this.board = board;
     }
 
     /**
      * Return the current board.
      */
-    Board getBoard() {
+    The2048Board getBoard() {
         return board;
     }
 
     /**
      * Manage a new shuffled board.
      */
-
-    int [] boardNumber = new int[16];
-
-    void beginBoardList(){
-        int set1 = (int)(Math.random()*15);
-        int set2 = (int)(Math.random()*15);
-        int set3 = (int)(Math.random()*3);
-        while (set1 == set2){
-            set2 = (int)(Math.random() * 15);
+    private int[] beginBoardList() {
+        int[] boardNumber = new int[16];
+        int set1 = (int) (Math.random() * 15);
+        int set2 = (int) (Math.random() * 15);
+        while (set1 == set2) {
+            set2 = (int) (Math.random() * 15);
         }
-        if (set3 == 0) {
-            boardNumber[set1] = 2;
-            boardNumber[set2] = 2;
-        }else if (set3 == 1){
-            boardNumber[set1] = 2;
-            boardNumber[set2] = 4;
-        }else if (set3 == 2){
-            boardNumber[set1] = 4;
-            boardNumber[set2] = 2;
-        }else {
-            boardNumber[set1] = 4;
-            boardNumber[set2] = 4;
-        }
+        boardNumber[set1] = ((int) (Math.random() * 1) + 1) * 2;
+        boardNumber[set2] = ((int) (Math.random() * 1) + 1) * 2;
+        return boardNumber;
     }
 
-    BoardManager(int gridSize) {
-        beginBoardList();
-        List<Tile> tiles = new ArrayList<>();
-        final int numTiles = gridSize * gridSize;
+    The2048BoardManager() {
+        int[] boardNumber = beginBoardList();
+        List<TofeTile> tiles = new ArrayList<>();
+        final int numTiles = The2048Board.numTiles;
         for (int tileNum = 0; tileNum != numTiles; tileNum++) {
             if (boardNumber[tileNum] == 2) {
-                tiles.add(new Tile(1, gridSize));
-            }else if (boardNumber[tileNum] == 4) {
-                tiles.add(new Tile(3, gridSize));
-            }else{
-                tiles.add(new Tile(15, gridSize));
+                tiles.add(new TofeTile(2));
+            } else if (boardNumber[tileNum] == 4) {
+                tiles.add(new TofeTile(4));
+            } else {
+                tiles.add(new TofeTile(0));
             }
         }
-
-        this.board = new Board(tiles, gridSize);
+        this.board = new The2048Board(tiles);
     }
 
-    void appearNewTile(){
-        int appearPosition = (int)(Math.random()*15);
-        int appearNumber = (int)(Math.random());
-        while (boardNumber[appearPosition] != 0){
-            appearPosition = (int)(Math.random()*15);
+    void movingVertical(boolean down){
+        The2048Board mergedBoard = mergeVertical(down);
+        Integer[] blankPositions = mergedBoard.getBlankPosition();
+        if (blankPositions.length != 0){
+            int randomPos = blankPositions[(int) (Math.random() * blankPositions.length)];
+            TofeTile randomTile = new TofeTile(((int) (Math.random() * 1) + 1) * 2);
+            mergedBoard.setTile(randomPos/The2048Board.numRows, randomPos%The2048Board.numCols, randomTile);
         }
-        boardNumber[appearPosition] = appearNumber;
+        this.board = mergedBoard;
     }
 
-    void recreateBoard(){
-        List<Tile> tiles = new ArrayList<>();
-        for (int i = 0; i!=16; i++){
-            tiles.add(new Tile(boardNumber[i]-1, 4));
-            //要保证boardNumber[i]－1 对应png的数字 重点需解决！！
-        }
-    }
-
-    /*
-    BoardManager(int gridSize) {
-        List<Tile> tiles = new ArrayList<>();
-        board.beginBoardList();
-        final int numTiles = gridSize * gridSize;
-        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-            if (board.boardNumber[tileNum] == 2) {
-                tiles.add(new Tile(2, gridSize));
-            }else if (board.boardNumber[tileNum] == 4) {
-                tiles.add(new Tile(4, gridSize));
-            }else{
-                tiles.add(new Tile(16, gridSize));
+    private The2048Board mergeVertical(boolean down) {
+        TofeTile[] resultingTiles = new TofeTile[16];
+        for (int column = 0; column < 4; column++) {
+            TofeTile[] mergedTiles = new Merge2048(board.getCol(column, down)).merge();
+            if (!down) {
+                for (int row = 1; row < 4; row++) {
+                    resultingTiles[row * 4 + column] = mergedTiles[row];
+                }
+            } else {
+                for (int row = 1; row < 4; row++) {
+                    resultingTiles[row * 4 + column] = mergedTiles[3 - row];
+                }
             }
         }
+        return new The2048Board(Arrays.asList(resultingTiles));
 
-        this.board = new Board(tiles, gridSize);
     }
-     */
+
+//    void appearNewTile(){
+//        int appearPosition = (int)(Math.random()*15);
+//        int appearNumber = (int)(Math.random());
+//        while (boardNumber[appearPosition] != 0){
+//            appearPosition = (int)(Math.random()*15);
+//        }
+//        boardNumber[appearPosition] = appearNumber;
+//    }
+//
+//    void recreateBoard(){
+//        List<Tile> tiles = new ArrayList<>();
+//        for (int i = 0; i!=16; i++){
+//            tiles.add(new Tile(boardNumber[i]-1, 4));
+//            //要保证boardNumber[i]－1 对应png的数字 重点需解决！！
+//        }
+//    }
+
     /**
      * Return whether the tiles are in row-major order.
      *
@@ -117,94 +118,25 @@ class The2048BoardManager implements Serializable {
      */
     boolean puzzleSolved() {
         boolean solved = true;
-
-        int tileId = 1;
-        for (Tile tile: this.board) {
-            solved = (tile.getId() == tileId);
-            tileId++;
+        for (TofeTile tile : this.board) {
+            solved = (tile.getValue() != 0);
             if (!solved) break;
         }
         return solved;
     }
 
     /**
-     * Return whether any of the four surrounding tiles is the blank tile.
-     *
-     * @param position the tile to check
-     * @return whether the tile at position is surrounded by a blank tile
-     */
-    boolean isValidTap(int position) {
-
-        int row = position / board.getNumRows();
-        int col = position % board.getNumCols();
-        int blankId = board.numTiles();
-        Tile above = row == 0 ? null : board.getTile(row - 1, col);
-        Tile below = row == board.getNumRows() - 1 ? null : board.getTile(row + 1, col);
-        Tile left = col == 0 ? null : board.getTile(row, col - 1);
-        Tile right = col == board.getNumCols() - 1 ? null : board.getTile(row, col + 1);
-        return (below != null && below.getId() == blankId)
-                || (above != null && above.getId() == blankId)
-                || (left != null && left.getId() == blankId)
-                || (right != null && right.getId() == blankId);
-    }
-
-    /**
-     * Find and return an array of the row and col of blankTile.
-     *
-     * @return An int array of the row and col of blankTile.
-     */
-    private int[] blankTilePos(){
-        int blankId = board.numTiles();
-        int row1 = 0;
-        int col1 = 0;
-        for (int r = 0; r != board.getNumRows(); r++) {
-            for (int c = 0; c != board.getNumCols(); c++) {
-                if (board.getTile(r, c).getId() == blankId) {
-                    row1 = r;
-                    col1 = c;
-                }
-            }
-        }
-        return new int[]{row1, col1};
-    }
-
-    /**
-     * Upon receiving a valid tap, switch the position of the
-     * blank tile and the tile at position.
-     *
-     * @param position the position
-     */
-    void touchMove(int position, boolean ifUndo) {
-        int row = position / board.getNumRows();
-        int col = position % board.getNumCols();
-
-        int[] position2 = blankTilePos();
-        if (!ifUndo) moveHistory();
-        board.swapTiles(row, col, position2[0], position2[1]);
-    }
-
-    /**
-     * get the exactly position of method blankTilePos().
-     *
-     * @return An int blankTile position.
-     */
-    int blankTilePosition(){
-        return blankTilePos()[0] * board.getNumRows() + blankTilePos()[1];
-    }
-
-    /**
      * the method will be used in Class MovementController.
-     *
      */
-    void undo(){
-        touchMove(board.historyStack.pop(), true);
-        board.setMaxUndoTime(board.getMaxUndoTime()-1);
+    void undo() {
+
     }
 
-    /**
-     * every time when user makes a move, historyStack will add the blankTile position.
-     *
-     */
-    private void moveHistory(){
-        board.historyStack.push(blankTilePosition());
-    }
+//    /**
+//     * every time when user makes a move, historyStack will add the blankTile position.
+//     *
+//     */
+//    private void moveHistory(){
+//        board.historyStack.push(blankTilePosition());
+//    }
+}
