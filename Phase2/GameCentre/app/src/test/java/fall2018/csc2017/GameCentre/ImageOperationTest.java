377be643421ca5b7cdb4c11ada97622e71c452d7
dpcjzpeter <31875874+dpcjzpeter@users.noreply.github.com>
2018-11-27 00:56:42 -0500

    Added a unit test support plugin: Robolectric. Created a sample unit test with few starting lines.

---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ImageOperationTest.java
@@ -0,0 +1,10 @@
+package fall2018.csc2017.GameCentre;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ImageOperationTest {
+
+
+}
